a310b853c408ea72c93363e9d7fa0608e34ecea4
Gualberto Junio Moreira Silva - 42113921 <juniojms13@gmail.com>
2022-05-24 21:16:24 -0300

    Commit inicial

---
+++ Pratica3/src/Exercicio1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		int num1,num2,num3;        
+        Scanner sc = new Scanner(System.in);        
+        
+        System.out.println("Digite 3 valores");
+        System.out.println("Valor 1");
+        num1 = sc.nextInt();
+        System.out.println("Valor 2");
+        num2 = sc.nextInt();
+        System.out.println("Valor 3");
+        num3 = sc.nextInt();
+        
+        double total = (num1 + num2 + num3)/3;
+        System.out.println("A média é " + total);
+        sc.close();
+
+	}
+
+}
